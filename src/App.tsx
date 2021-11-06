8c9040b98855fa6165ac04d596fa8095bb435930
Arsenii Netrebenko <boecrejha@gmail.com>
2021-11-06 21:45:46 +0200

    test edit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Firebase</p>
+				<p>Hello actions on Github for auto Deployment to Firebase!</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
